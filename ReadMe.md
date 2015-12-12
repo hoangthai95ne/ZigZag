a654b613dc0c156cc31610dd2a06be77ee885997
Hoàng Thái <Cayenne@Hoang-Thai-IM.local>
2015-12-12 19:11:48 +0700

    first commit

---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+ZigZag written By Hoang Thai
---
+++ ZigZag/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ZigZag
+//
+//  Created by Hoàng Thái on 12/12/15.
+//  Copyright © 2015 HOANGTHAI. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    printf("     +       +       +\n");
+    printf("    + +     + +     +\n");
+    printf("   +   +   +   +   +\n");
+    printf("  +     + +     + +\n");
+    printf(" +       +       +\n");
+    
+}
+
+
+@end
